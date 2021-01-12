ebffdfebcbfb523e39b348c94cfad6b804f5cf64
Sarfaraz <ansarisarfarazlive.com>
2021-01-13 00:55:28 +0530

    make it better

--- public/js/createMacros.js
+++ public/js/createMacros.js
@@ -1,35 +1,46 @@
 let btn = document.querySelector('.btn');
 let title = document.querySelector('.title');
 let spans = document.getElementsByTagName('span');
+let hide = document.querySelector('#hidden');
 
-
-btn.addEventListener('click',function(){
+function generateMacros(genderFactor,proteinFactor)
+{
     let name = document.getElementsByClassName('form-control')[0].value;
-    let weight = document.getElementsByClassName('form-control')[2].value;
-    let bmr = weight * 15; 
-    let protein = Math.round(weight * 1);
-    let fats = Math.round(weight * 0.35);
-    let totalCalories;
-    let carbs;
+        let weight = document.getElementsByClassName('form-control')[2].value;
+        let bmr = Math.round(weight * 2.2 * genderFactor);
+        let protein = Math.round(weight * 2.2 * proteinFactor);
+        let fats = Math.round(weight * 2.2 * 0.35);
+        let totalCalories;
+        let carbs;
 
-    if(title.innerText==="Build-Muscle")
-    {
-        spans[1].innerHTML = "Muscle Building";
-        totalCalories = bmr + 200;
-        spans[2].innerHTML = totalCalories;
-        carbs = Math.round((totalCalories - ((protein * 4)+(fats * 9))) / 4);
-        spans[4].innerHTML = carbs;
-    }   
-    else if(title.innerText==="Loose-Fat")
-    {
-        spans[1].innerHTML = "Fat Loss";
-        totalCalories = bmr - 200;
-        spans[2].innerHTML = totalCalories;
-        carbs = Math.round((totalCalories - ((protein * 4)+(fats * 9))) / 4);
-        spans[4].innerHTML = carbs;
+        if (title.innerText === "Build-Muscle") {
+            spans[1].innerHTML = "Muscle Building";
+            totalCalories = bmr + 200;
+            spans[2].innerHTML = totalCalories;
+            carbs = Math.round((totalCalories - ((protein * 4) + (fats * 9))) / 4);
+            spans[4].innerHTML = carbs;
+        }
+        else if (title.innerText === "Loose-Fat") {
+            spans[1].innerHTML = "Fat Loss";
+            totalCalories = bmr - 200;
+            spans[2].innerHTML = totalCalories;
+            carbs = Math.round((totalCalories - ((protein * 4) + (fats * 9))) / 4);
+            spans[4].innerHTML = carbs;
+        }
+        spans[3].innerHTML = protein;
+        spans[5].innerHTML = fats;
+    
+}
+
+btn.addEventListener('click', function () {
+    if (hide.value === "male") 
+    {   
+        generateMacros(15,1);
     }
-    spans[3].innerHTML = protein;
-    spans[5].innerHTML = fats;
+    else
+    {
+        generateMacros(12,0.8)
+    }
 })
 
 
--- routes.js
+++ routes.js
@@ -7,11 +7,23 @@ router.get('/',(req,res)=>{
 
 router.get('/:type',(req,res)=>{
     const {type} = req.params;
-
-    if((type==="Build-Muscle") || (type==="Loose-Fat"))
+    if(type ==="Build-Muscle" || type ==="Loose-Fat")
     {
-        res.render('name',{type:type});
+        res.render('gender',{type : type});
     }
+})
+
+router.get('/:type/:gender',(req,res)=>{
+    const {type,gender} = req.params;
+
+    if((type==="Build-Muscle" || gender==="male") || (type==="Loose-Fat" || gender==="male") || (type==="Build-Muscle" || gender==="female") || (type==="Loose-Fat" || gender==="female"))
+    {
+        res.render('name',{type:type, gender:gender});
+    }
+})
+
+router.get('/:type/:gender/workout-plans',(req,res)=>{
+    res.render('workouts');
 })
 
 module.exports = router;
